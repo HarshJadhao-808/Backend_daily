a260d290c8e7c40c36c5c296296c83bb1d1ff641
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-09-13 14:58:04 +0530

    core modules

---
+++ Core_modules/Fs.js
@@ -0,0 +1,20 @@
+import fs, { readFile } from 'fs'
+
+// fs.writeFileSync("notes.txt", "hello txt", (err) => {
+// 	if (err) throw err;
+
+// 	console.log("file created");
+// });
+
+
+// fs.renameSync("notes.txt","newnotes.txt")
+
+// fs.appendFileSync("newnotes.txt","\nHello new")
+
+// fs.readFile("newnotes.txt","utf-8",(err,data)=>{
+//     if(err) throw err
+
+//     console.log(data)
+// })
+
+// fs.unlinkSync("newnotes.txt")
---
+++ Core_modules/Os.js
@@ -0,0 +1,6 @@
+import os from 'os'
+
+console.log(os.arch())
+console.log(os.hostname())
+console.log(os.totalmem())
+console.log(os.freemem())
