c6da0061f96faf8bbfa232bf3d892839218f20d6
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-09-19 14:45:58 +0530

    crud completed finally

--- Express/crud.js
+++ Express/crud.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import fs from 'fs'
+import fs, { read } from 'fs'
 import nodemon from 'nodemon'
 import { json } from 'stream/consumers'
 dotenv.config()
@@ -19,21 +19,38 @@ const writedb = (data) => {
 }
 
 app.get("/books",(req,res)=>{
-    const db = readdb()
-    res.send(db.books)
+    let data = JSON.parse(readdb())
+    res.send(data)
 })
-
-
 app.post("/books",(req,res)=>{
-     const db = readdb()
-    const {author,book}=req.body
-  let  new_book = {id:Date.now() ,author,book}
-    
-    db.books.push(new_book)
-    writedb(db)
-    res.json(new_book)
-     
+        let data = readdb()
+        let  {title,author} = req.body
+        let new_obj = {id:Math.floor(Math.random() * 1000) , title , author}
+        data.books.push(new_obj)
+        writedb(data)
+        res.send(data)
 })
+
+app.put("/books/:id",(req,res)=>{
+        let data = readdb()
+        const { id }= req.params
+        let {title,author}=req.body
+        let index = data.books.findIndex((el)=> el.id == id)
+        data.books[index]={...data.books[index], title , author}
+        writedb(data)
+        res.send(data)
+})
+
+
+app.delete("/books/:id",(req,res)=>{
+    let data = readdb()
+    const {id} = req.params;
+    let index = data.books.findIndex((el)=> el.id == id)
+    data.books.splice(index,1)
+    writedb(data)
+    res.send(data)
+})
+
 
 let port = process.env.CRUDPORT
 app.listen(port,()=>{
