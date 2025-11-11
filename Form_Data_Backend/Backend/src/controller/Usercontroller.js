561b292498a7e037c1deff8206b2314e8566b2cb
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-11-11 15:18:49 +0530

    backend_branch created

--- Form_Data_Backend/Backend/src/controller/Usercontroller.js
+++ Form_Data_Backend/Backend/src/controller/Usercontroller.js
@@ -5,12 +5,13 @@ import bcrypt from 'bcrypt'
 export const Signupfun = async() => {
     const {name,email,password,role} = req.body
 
-    const hashed_password = bcrypt.hash()
+    const hashed_password = bcrypt.hash(password,10)
     let data = await mongoose.create({
         name,
         email,
-        password,
+        hashed_password,
         role
     })
+    res.json({message:"User Created",data})
     
 }
