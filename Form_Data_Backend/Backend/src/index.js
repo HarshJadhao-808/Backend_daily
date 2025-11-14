9dce63da83073f0061c1c8e021db64fe522592a7
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-11-14 18:42:28 +0530

    Forms Remaining

--- Form_Data_Backend/Backend/src/index.js
+++ Form_Data_Backend/Backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import connectDb from './config/db.js'
 import cors from 'cors'
 import UserRouter from './routes/Userroutes.js'
+import User from './models/UserModel.js'
+import tokencheck from './middleware/Authcontroller.js'
 dotenv.config()
 const app = express()
 app.use(cors())
@@ -10,10 +12,6 @@ app.use(express.json())
 connectDb(process.env.Mongo_Url);
 
 app.use("/user",UserRouter)
-
-
-
-
 
 const Port=process.env.PORT
 app.listen(Port,()=>{
--- Form_Data_Backend/Frontend/src/components/Form.jsx
+++ Form_Data_Backend/Frontend/src/components/Form.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Personal_Info from './Form_Pages/Personal_Info.jsx'
+import { useState } from 'react'
+import Educational_Info from './Form_Pages/Educational_Info.jsx'
+import Professional_info from './Form_Pages/Professional_info.jsx'
+import Identity_Info from './Form_Pages/Identity_Info.jsx'
+import Review from './Form_Pages/Review.jsx'
 
 const Form = () => {
+  const [page,setPage] = useState(0)
+  const page_no = [
+    <Personal_Info/>,
+    <Educational_Info/>,
+    <Professional_info/>,
+    <Identity_Info/>,
+    <Review/>
+  ]
   return (
     <div>
-        
+      {page_no[page]}
     </div>
   )
 }
