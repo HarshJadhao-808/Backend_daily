abd87bf4ae50ca78e9778907bd7726cd112547de
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-11-15 20:23:28 +0530

    tailwind imported

--- Form_Data_Backend/Frontend/src/components/Navbar.jsx
+++ Form_Data_Backend/Frontend/src/components/Navbar.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Signup</Link>
-    </div>
-  )
+		<nav class="flex justify-between px-2 mb-2 mx-4 my-2 sm:my-5 sm:mx-7 my:1 sm border-2 border-[#386BAC]  sm:py-4 sm:px-9">
+			<div>
+				<Link to="/">
+					<div class="flex align-middle  h-[100%] ">
+						<img class="w-10 sm:w-14" src="../../src/assets/home.svg" alt="" />
+					</div>
+				</Link>
+			</div>
+			<div class="flex items-center">
+				<Link
+					className="authbuttons"
+					class=" border-red-200 px-2 text-0xl sm:px-9 sm:text-2xl "
+					to="/login"
+				>
+					Login
+				</Link>
+				<Link
+					className="authbuttons"
+					class="px-2 text-0xl sm:px-9  sm:text-2xl"
+					to="/signup"
+				>
+					Signup
+				</Link>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar
